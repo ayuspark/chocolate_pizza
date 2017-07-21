65d491ff6e821263565cd82637c3e687a7c338da
ayuspark <lola.luyaowang@gmail.com>
2017-07-21 16:53:22 -0700

    finished all layout, waiting for font styling

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-
+//
+// <label class="strikethrough" for="milk"><input type="checkbox" name="milk">1 1/2 cups milk</label>
+// <label class="strikethrough"><input type="checkbox">1/2 mascarpone</label>
+// <label class="strikethrough"><input type="checkbox">1/2 tsp pink salt</label>
+// <label class="strikethrough"><input type="checkbox">1 lbs Black Mission Figs</label>
+// <label class="strikethrough"><input type="checkbox">1/2 cup brown sugar</label>
+// <label class="strikethrough"><input type="checkbox">2-4 tbsp water</label>
+// <label class="strikethrough"><input type="checkbox">1 1/2 cups heavy cream</label>
+// <label class="strikethrough"><input type="checkbox">1/3 granulated sugar</label>
+// <label class="strikethrough"><input type="checkbox">2 egg yolks</label>
+// <label class="strikethrough"><input type="checkbox">1 lemon, juiced</label>
+// <label class="strikethrough"><input type="checkbox">2 tbsp butter</label>
+// <label class="strikethrough"><input type="checkbox">1 cup honey roasted pecans, roughly chopped</label>
